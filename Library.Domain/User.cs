7c5c4b134c233e839ce36714c516cf735e0c7024
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2025-08-19 16:59:13 +0200

    Add User class

---
+++ Library.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime RegisteredDate { get; set; } = DateTime.UtcNow;
+    }
+}
