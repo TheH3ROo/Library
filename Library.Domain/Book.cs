3b51403c8c0ff2ba7f1407cbe939c27f6c3dc46b
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2025-08-19 16:58:39 +0200

    Fix Book properties

--- Library.Domain/Book.cs
+++ Library.Domain/Book.cs
@@ -6,8 +6,7 @@
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string ISBN { get; set; } = string.Empty;
-        public int PublicationYear { get; set; }
+        public int PublishedYear { get; set; }
         public bool IsAvailable { get; set; } = true;
-        public bool IsBorrowed { get; set; } = true;
     }
 }
