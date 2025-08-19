5ef03610f32bc27339203738266bed4e469b47b9
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2025-08-19 11:45:38 +0200

    BorrowDomain: throw when book not available (green test)

--- Library.Domain/BorrowDomain.cs
+++ Library.Domain/BorrowDomain.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library.Domain
+﻿namespace Library.Domain
 {
     public static class BorrowDomain
     {
         public static void Borrow(Book book, Guid userId, DateTime now)
         {
-            //
+            if (book is null) throw new ArgumentNullException(nameof(book), "Book cannot be null.");
+
+            if (!book.IsAvailable) throw new InvalidOperationException("Book not available.");
         }
     }
 }
