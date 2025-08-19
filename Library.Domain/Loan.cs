47f87ea5b04a4e8a524e1dca14f0c2066b6c05d9
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2025-08-19 16:58:51 +0200

    Add Loan class

---
+++ Library.Domain/Loan.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Domain
+{
+    public class Loan
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime LoanDate { get; set; } = DateTime.UtcNow;
+        public DateTime? ReturnDate { get; set; }
+    }
+}
