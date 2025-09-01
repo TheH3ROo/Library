aa56c4ba3ba0942e6807d6183fafc804aa8da03e
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2025-09-01 19:45:34 +0200

    Add requirements for User signup

--- Library.Application/Users/UserDtos.cs
+++ Library.Application/Users/UserDtos.cs
@@ -1,8 +1,16 @@
-﻿namespace Library.Application.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Application.Users
 {
     public sealed class UserCreateDto
     {
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression(@".*\S.*", ErrorMessage = "A név nem lehet üres.")]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        [MaxLength(254)]
         public string Email { get; set; } = string.Empty;
     }
 }
